fixed normalization and 4 momenta

dark_gen.py
@@ -22,26 +22,26 @@ def main(argv):
 	####################
 	# User specified
 	parser = argparse.ArgumentParser(description="Generate dark nu events")
-	parser.add_argument("--mzprime", type=float, help="Z' mass", default=1.25)
-	parser.add_argument("--M4", type=float, help="mass of the fourth neutrino", default=0.100)
+	parser.add_argument("--mzprime", type=float, help="Z' mass", default=0.03)
+	parser.add_argument("--M4", type=float, help="mass of the fourth neutrino", default=0.420)
 	parser.add_argument("--M5", type=float, help="mass of the fifth neutrino", default=1e5)
 	parser.add_argument("--M6", type=float, help="mass of the sixth neutrino", default=1e5)
 
-	parser.add_argument("--UMU4", type=float, help="Umu4", default=np.sqrt(13.6e-8))
+	parser.add_argument("--UMU4", type=float, help="Umu4", default=np.sqrt(9.0e-7))
 	parser.add_argument("--UMU5", type=float, help="Umu5", default=np.sqrt(26.5e-8))
 	parser.add_argument("--UMU6", type=float, help="Umu6", default=np.sqrt(123.0e-8*0.0629))
-	parser.add_argument("--GPRIME", type=float, help="gprime", default=np.sqrt(4*np.pi*0.32))
+	parser.add_argument("--GPRIME", type=float, help="gprime", default=np.sqrt(4*np.pi*1/4.0))
 
-	parser.add_argument("--UTAU4", type=float, help="UTAU4", default=np.sqrt(7.8e-4)*0)
-	parser.add_argument("--UTAU5", type=float, help="UTAU5", default=np.sqrt(7.8e-4)*0)
-	parser.add_argument("--UTAU6", type=float, help="UTAU6", default=np.sqrt(7.8e-4)*0)
-	parser.add_argument("--CHI", type=float, help="CHI", default=np.sqrt(5.94e-4))
+	parser.add_argument("--UTAU4", type=float, help="UTAU4", default=0)
+	parser.add_argument("--UTAU5", type=float, help="UTAU5", default=0)
+	parser.add_argument("--UTAU6", type=float, help="UTAU6", default=0)
+	parser.add_argument("--CHI", type=float, help="CHI", default=np.sqrt(2e-10/const.alphaQED)/const.cw)
 
 	parser.add_argument("--UD4", type=float, help="UD4", default=1.0)
 	parser.add_argument("--UD5", type=float, help="UD5", default= 1.0)
 	parser.add_argument("--UD6", type=float, help="UD6", default= 1.0)
 
-	parser.add_argument("--ldecay", type=float, help="ctau of the fourth neutrino", default=0.05)
+	parser.add_argument("--ldecay", type=float, help="ctau of the fourth neutrino in cm", default=0.00)
 	parser.add_argument("--exp", type=str, help="experiment", choices=["charmii",
 																		"minerva_le",
 																		"minerva_me",
@@ -56,15 +56,21 @@ def main(argv):
 	parser.add_argument("--noplot", help="no plot", action="store_true")
 	parser.add_argument("--HNLtype", type=int, help="HNLtype: 1 is DIRAC, 0 is MAJORANA", choices=[0, 1], default=0)
 	
-	parser.add_argument("--nevents", type=int, help="number of events to generate", default=100)
+	parser.add_argument("--neval", type=int, help="number of evaluations of integrand", default=1e5)
+	parser.add_argument("--nint", type=int, help="number of adaptive iterations", default=20)
+	parser.add_argument("--neval_warmup", type=int, help="number of evaluations of integrand in warmup", default=1e3)
+	parser.add_argument("--nint_warmup", type=int, help="number of adaptive iterations in warmup", default=10)
+
+	parser.add_argument("--hepevt_events", type=int, help="number of events to accept in HEPEVT format", default=1e2)
+
 	args = parser.parse_args()
 
 	##########################
 	# MC evaluations and iterations
-	MC.NEVAL_warmup = 1e3
-	MC.NINT_warmup = 100
-	MC.NEVAL = 1e4
-	MC.NINT  = 100
+	MC.NEVAL_warmup = args.neval_warmup
+	MC.NINT_warmup = args.nint_warmup
+	MC.NEVAL = args.neval
+	MC.NINT  = args.nint
 		
 	#########################
 	# Set BSM parameters
@@ -193,13 +199,15 @@ def main(argv):
 		# regime = bag['flags']
 
 
-	print(np.sum(bag['w']))
-	print(np.size(bag['w']))
-	print(Cfv.dot4(bag['P2_decay'],bag['P2_decay']))
+
+	print(np.sum(bag['w'])/const.GeV2_to_cm2)
+	print(np.sum(bag['w'])*const.NAvo*12e6*13e20)
 	############################################################################
 	# Print events to file -- currently in data/exp/m4____mzprime____.dat 
 	#############################################################################
-	printer.print_events_to_pandas(PATH_data, bag, args.nevents, BSMparams, l_decay_proper=args.ldecay)
+	printer.print_events_to_pandas(PATH_data, bag, BSMparams, l_decay_proper=args.ldecay)
+	# HEPEVT_events = args.hepevt_events
+	# printer.print_unweighted_events_to_HEPEVT(PATH_data, bag, HEPEVT_events, BSMparams, l_decay_proper=args.ldecay)
 
 if __name__ == "__main__":
 	try:

dark_news/const.py
@@ -181,15 +181,8 @@ def F3_WEAK(Q2):
 	return f
 
 
-
-def Fcoh(Q,MA):
-	a = 0.523/0.197 # GeV^-1
-	r0 = 1.03*(MA**(1.0/3.0))/0.197 # GeV^-1
-	return 3.0*np.pi*a/(r0**2 + np.pi**2 * a**2) * (np.pi*a *(1.0/np.tanh(np.pi*a*Q))*np.sin(Q*r0) - r0*np.cos(Q*r0))/(Q*r0*np.sinh(np.pi*Q*a))
-
-
+a = 0.523/0.197 # GeV^-1
 def FEMcoh(Q,MA):
-	a = 0.523/0.197 # GeV^-1
 	r0 = 1.03*(MA**(1.0/3.0))/0.197 # GeV^-1
 	return 3.0*np.pi*a/(r0**2 + np.pi**2 * a**2) * (np.pi*a *(1.0/np.tanh(np.pi*a*Q))*np.sin(Q*r0) - r0*np.cos(Q*r0))/(Q*r0*np.sinh(np.pi*Q*a))
 

dark_news/exp.py
@@ -103,13 +103,13 @@ class experiment():
 			self.MATERIALS_NAME=['H1', 'C12', 'O16', 'Cu29', 'Zn30', 'Pb208']
 			self.MATERIALS_A=[1, 12, 16, 63.546, 65.38, 207.2]
 			self.MATERIALS_Z=[1, 6, 8, 29, 30, 82]
-			self.MATERIALS_MASS =[
+			self.MATERIALS_TOT_MASS =[
 								2.9e6*2.0/18.0,
 								0,
 								2.9e6*16.0/18.0,
 								0,
 								12.9e6] # grams
-			self.TOT_MASS =np.sum(self.MATERIALS_MASS)
+			self.TOT_MASS =np.sum(self.MATERIALS_TOT_MASS)
 
 		elif self.EXP_FLAG == ND280_nubar:
 			self.FLUXFILE="fluxes/T2Kflux2016/t2kflux_2016_nd280_minus250kA.txt"
@@ -121,14 +121,13 @@ class experiment():
 			self.MATERIALS_NAME=['H1', 'C12', 'O16', 'Cu29', 'Zn30', 'Pb208']
 			self.MATERIALS_A=[1, 12, 16, 63.546, 65.38, 207.2]
 			self.MATERIALS_Z=[1, 6, 8, 29, 30, 82]
-			self.MATERIALS_MASS =[
+			self.MATERIALS_TOT_MASS =[
 								2.9e6*2.0/18.0,
 								0,
 								2.9e6*16.0/18.0,
 								0,
 								12.9e6] # grams
-			self.TOT_MASS =np.sum(self.MATERIALS_MASS)
-
+			self.TOT_MASS =np.sum(self.MATERIALS_TOT_MASS)
 		
 		else:
 			print('ERROR! No experiment chosen.')

dark_news/integrands.py
@@ -97,11 +97,12 @@ class cascade(vg.BatchIntegrand):
 			# dsigma *= 16*F2*FA*Q2*(2*Power(MA,2) + Power(Mn,2) + Q2 - 2*s) + 8*F1*(4*FA*Power(MA,2)*Q2 - (Power(Mn,2) + Q2)*(F2*Power(Mn,2) - 2*(F2 + FA)*Q2) - 4*FA*Q2*s) + 8*Power(FA,2)*(2*Power(MA,4) + Q2*(Power(Mn,2) + Q2) + 4*Power(MA,2)*(Power(Mn,2) + Q2 - s) - 2*(Power(Mn,2) + Q2)*s + 2*Power(s,2)) + 8*Power(F1,2)*(Q2*(Power(Mn,2) + Q2) + 2*(Power(MA,4) - (2*Power(MA,2) + Power(Mn,2) + Q2)*s + Power(s,2))) + (Power(F2,2)*(4*Power(MA,4)*Q2 - 4*Power(MA,2)*(Power(Mn,4) - Power(Mn,2)*Q2 + 2*Q2*(-Q2 + s)) + Q2*(Power(Mn,4) + Power(Mn,2)*(Q2 - 4*s) + 4*s*(-Q2 + s))))/Power(MA,2)
 
 		else:
-			F1 = const.Fcoh(np.sqrt(Q2),MA)
+			F1 = const.FEMcoh(np.sqrt(Q2),MA)
 			F2 = 0
 			FA = 0
 			dsigma *= Z*Z*4.0*Power(F1,2)*(4*Power(MA,4) + Power(Mn,4) + Power(Mn,2)*Q2 - 4*(2*Power(MA,2) + Power(Mn,2) + Q2)*s + 4*Power(s,2))
 			# dsigma *= Z*Z*4*Power(F1,2)*(4*Power(MA,4) + Power(Mn,4) + Power(Mn,2)*Q2 - 4*(2*Power(MA,2) + Power(Mn,2) + Q2)*s + 4*Power(s,2))
+			
 		betaPS = np.sqrt(1.0 - 2*(Mn**2 + MA**2)/s + (Mn**2 - MA**2)**2/s**2)
 		dsigma *= betaPS/(32.0*np.pi**2)
 		
@@ -116,7 +117,6 @@ class cascade(vg.BatchIntegrand):
 		#######################
 		## Heavy N Decay
 		cost = -1.0 + (2.0) * x[:, 2]
-
 		dgamma = 2*UD4**2*(params.Ue4**2 + params.Umu4**2 + params.Utau4**2)*alphaD*4*np.pi*(Mn**2 - 2*Mzprime**2 + Mn**4/Mzprime**2)/2.0/Mn/32.0/np.pi**2*(1.0-Mzprime**2/Mn**2) 
 	
 		############# If Dirac, include more stuff! ###################
@@ -132,6 +132,7 @@ class cascade(vg.BatchIntegrand):
 		dgammaZprime *= 2*np.pi*2 # integral over angles
 
 		return dsigma*self.flux(Enu)*dgamma*dgammaZprime
+		# return dgamma*dgammaZprime
 		# return const.Gf**2*(2.0*Enu*const.Me)/4.0/np.pi *flux(Enu)
 
 
@@ -505,9 +506,9 @@ class threebody(vg.BatchIntegrand):
 
 def cascade_phase_space(samples=None, MC_case=None, w=None, I=None):
 
-	Q2p = np.array(samples[0])
-	Enup =  np.array(samples[1])
-	costZ = np.array(samples[2])
+	Q2p = samples[0]
+	Enup =  samples[1]
+	costZ = samples[2]
 	sample_size = np.shape(Enup)[0]
 
 	MA = MC_case.MA
@@ -620,17 +621,6 @@ def cascade_phase_space(samples=None, MC_case=None, w=None, I=None):
 	phie   = Cfv.random_generator(sample_size, 0.0, 2*np.pi)
 
 
-	# P1CM_decayZ = [ [E1CM_decayZ[i],
-	# 		  		p1CM_decayZ[i]*np.cos(phie[i])*np.sin(thetae[i]), 
-	# 		  		p1CM_decayZ[i]*np.sin(phie[i])*np.sin(thetae[i]), 
-	# 		  		p1CM_decayZ[i]*np.cos(thetae[i])] for i in range(0,np.size(thetae))]
-	
-	# P2CM_decayZ = [ [E2CM_decayZ[i],
-	# 		  		-p2CM_decayZ[i]*np.cos(phie[i])*np.sin(thetae[i]), 
-	# 		  		-p2CM_decayZ[i]*np.sin(phie[i])*np.sin(thetae[i]), 
-	# 		  		-p2CM_decayZ[i]*np.cos(thetae[i])] for i in range(0,np.size(thetae))]
-
-
 	P1CM_decayZ = Cfv.build_fourvec(E1CM_decayZ, p1CM_decayZ, cthetae, phie)
 	P2CM_decayZ = Cfv.build_fourvec(E1CM_decayZ, -p1CM_decayZ, cthetae, phie)
 
@@ -646,21 +636,33 @@ def cascade_phase_space(samples=None, MC_case=None, w=None, I=None):
 							-np.sqrt( EZ_LAB**2 - Mzprime**2)/EZ_LAB, 
 							costZ_LAB,  
 							phiZ_LAB)
+
 	##########################################################################
 
 	# return P3LAB, P2LAB_decay, P1LAB_decayZ, P2LAB_decayZ, P1LAB_decay, P4LAB
 	# returing dictionary
-	return  {"P1" : P1LAB,
-			"P2" : P2LAB,
-			"P3" : P3LAB,
-			"P4" : P4LAB,
-			"P1_decay" : P1LAB_decay, 
-			"P2_decay" : P2LAB_decay, 
-			"P3_decay" : P1LAB_decayZ, 
-			"P4_decay" : P2LAB_decayZ,
+	# return  {"P1" : P1LAB,
+	# 		"P2" : P2LAB,
+	# 		"P3" : P3LAB,
+	# 		"P4" : P4LAB,
+	# 		"P1_decay" : P1LAB_decay, 
+	# 		"P2_decay" : P2LAB_decay, 
+	# 		"P3_decay" : P1LAB_decayZ, 
+	# 		"P4_decay" : P2LAB_decayZ,
+	# 		"w" : w,
+	# 		"I" : I}
+	# returing dictionary
+	return  {"P_scatterer" : P1LAB, 
+			"P_target" : P2LAB, 
+			"P_outgoing_HNL" : P3LAB, 
+			"P_outgoing_target" : P4LAB, 
+			"P_HNL" : P1LAB_decay, 
+			"P_mediator" : P2LAB_decay, 
+			"P_out_nu" : P1LAB_decay, 
+			"P_em" : P1LAB_decayZ, 
+			"P_ep" : P2LAB_decayZ,
 			"w" : w,
 			"I" : I}
-	
 
 def three_body_phase_space(samples=None, MC_case=None, w=None, I=None):
 
@@ -795,14 +797,14 @@ def three_body_phase_space(samples=None, MC_case=None, w=None, I=None):
 								phiN_LAB)	
 
 		# returing dictionary
-		return  {"P1" : P1LAB,
-				"P2" : P2LAB,
-				"P3" : P3LAB,
-				"P4" : P4LAB,
-				"P1_decay" : P1LAB_decay, 
-				"P2_decay" : P2LAB_decay, 
-				"P3_decay" : P3LAB_decay, 
-				"P4_decay" : P4LAB_decay,
+		return  {"P_scatterer" : P1LAB,
+				"P_target" : P2LAB,
+				"P_outgoing_HNL" : P3LAB,
+				"P_outgoing_target" : P4LAB,
+				"P_HNL" : P1LAB_decay, 
+				"P_out_nu" : P2LAB_decay, 
+				"P_em" : P3LAB_decay, 
+				"P_ep" : P4LAB_decay,
 				"w" : w,
 				"I" : I}
 	

dark_news/printer.py
@@ -16,14 +16,14 @@ from . import Cfourvec as Cfv
 from dark_news.geom import old_geometry_muboone
 from dark_news.fourvec import dot4
 
-def print_events_to_pandas(PATH_data, bag, TOT_EVENTS, BSMparams, l_decay_proper=0.0):
+def print_events_to_pandas(PATH_data, bag, BSMparams, l_decay_proper=0.0):
 	# events
-	pN   = bag['P3']
-	pnu   = bag['P2_decay']
-	pZ   = bag['P3_decay']+bag['P4_decay']
-	plm  = bag['P3_decay']
-	plp  = bag['P4_decay']
-	pHad = bag['P4']
+	pN   = bag['P_outgoing_HNL']
+	pnu   = bag['P_out_nu']
+	pZ   = bag['P_em']+bag['P_ep']
+	plm  = bag['P_em']
+	plp  = bag['P_ep']
+	pHad = bag['P_outgoing_target']
 	w = bag['w']
 	I = bag['I']
 	regime = bag['flags']
@@ -57,7 +57,8 @@ def print_events_to_pandas(PATH_data, bag, TOT_EVENTS, BSMparams, l_decay_proper
 			z_decay,]
 	
 	aux_df = pd.DataFrame(np.stack(aux_data, axis=-1), columns=columns_index)
-	aux_df.loc[:, 'weight'] = w
+	print(np.shape(np.stack(aux_data, axis=-1)))
+	# aux_df.loc[:, 'weight'] = w
 	# aux_df.loc[:, 'regime'] = regime
 
 
@@ -75,12 +76,12 @@ def print_events_to_pandas(PATH_data, bag, TOT_EVENTS, BSMparams, l_decay_proper
 # not relevant anymore. 
 def print_unweighted_events_to_HEPEVT(PATH_data, bag, TOT_EVENTS, BSMparams, l_decay_proper=0.0):
 	# events
-	pN   = bag['P3']
-	pnu   = bag['P2_decay']
-	pZ   = bag['P3_decay']+bag['P4_decay']
-	plm  = bag['P3_decay']
-	plp  = bag['P4_decay']
-	pHad = bag['P4']
+	pN   = bag['P_outgoing_HNL']
+	pnu  = bag['P_out_nu']
+	pZ   = bag['P_em']+bag['P_ep']
+	plm  = bag['P_em']
+	plp  = bag['P_ep']
+	pHad = bag['P_outgoing_target']
 	Mhad = np.sqrt(dot4(pHad, pHad))
 	w = bag['w']
 	I = bag['I']